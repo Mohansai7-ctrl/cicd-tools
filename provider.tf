9c89521aabc09aed86659c3205ca53e207a6b43f
Mohansai <bandarumohansai@gmail.com>
2024-11-23 21:25:23 +0530

    Updated the terraform scripts to create jenkins master and jenkins agent

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+
+    backend "s3" {
+        bucket = "mohan-remote-state"
+        dynamodb_table = "mohan-locking"
+        key = "expense-tools"
+        region = "us-east-1"
+
+    }
+
+    
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
